6bde193eb4ff2a5845ab902d91516894c1111b3d
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-12-13 14:42:59 -0300

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'home',
+})
+
+const online = useOnline()
+</script>
+
+<template>
+  <div>
+    <Logos mb-6 />
+    <ClientOnly>
+      <Suspense>
+        <p v-if="online">
+          Oi amigo
+        </p>
+        <div v-else text-gray:80>
+          You're offline
+        </div>
+        <template #fallback>
+          <div italic op50>
+            <span animate-pulse>Loading...</span>
+          </div>
+        </template>
+      </Suspense>
+      <template #fallback>
+        <div op50>
+          <span animate-pulse>...</span>
+        </div>
+      </template>
+    </ClientOnly>
+    <InputEntry />
+  </div>
+</template>
