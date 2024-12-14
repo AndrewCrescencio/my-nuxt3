b1572dc95fb6e3807e3ca625ab9cc1d781271d63
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-12-14 15:12:32 -0300

    fix: remove deleted components

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,7 +8,6 @@ const online = useOnline()
 
 <template>
   <div>
-    <Logos mb-6 />
     <ClientOnly>
       <Suspense>
         <p v-if="online">
@@ -31,6 +30,5 @@ const online = useOnline()
         </div>
       </template>
     </ClientOnly>
-    <InputEntry />
   </div>
 </template>
